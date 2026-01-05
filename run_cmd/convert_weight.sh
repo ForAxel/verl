1c492455ff8b8392c1f236ecb9e439d2def70eed
zhaoping <zpinghu@gmail.com>
2026-01-05 17:51:55 +0800

    backup run_cmd shells.

--- run_cmd/convert_weight.sh
+++ run_cmd/convert_weight.sh
@@ -11,8 +11,11 @@ export PYTHONPATH=${MEGATRON_PATH}:${VERL_PATH}:$PYTHONPATH
 # HF_MODEL_PATH='/home/dist/zhaoping/LLMs/Qwen3-30B-A3B'
 # DIST_CKPT_PATH='/home/dist/zhaoping/LLMs/MCORE/Qwen3-30B-A3B'
 
-HF_MODEL_PATH='/home/dist/zhaoping/LLMs/DeepSeek-V2-Lite'
-DIST_CKPT_PATH='/home/dist/zhaoping/LLMs/MCORE/DeepSeek-V2-Lite'
+# HF_MODEL_PATH='/home/dist/zhaoping/LLMs/DeepSeek-V2-Lite'
+# DIST_CKPT_PATH='/home/dist/zhaoping/LLMs/MCORE/DeepSeek-V2-Lite'
+
+HF_MODEL_PATH='/home/dist/zhaoping/LLMs/Qwen3-8B'
+DIST_CKPT_PATH='/home/dist/zhaoping/LLMs/MCORE/Qwen3-8B'
 
 # /home/dist/zhaoping/LLMs 存储了大量LLM模型参数
 python -u ../scripts/converter_hf_to_mcore.py --hf_model_path $HF_MODEL_PATH --output_path $DIST_CKPT_PATH
